9086c49c93e5497d6523c29b982eb815c9c09422
aliyeganehfard <76558514+aliyeganehfard@users.noreply.github.com>
2023-08-10 16:02:38 +0330

    Add auth server

---
+++ LARxury/oauth-server/src/test/java/ir/larxury/oauth/server/OauthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.larxury.oauth.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
