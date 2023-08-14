4e1d36ddc8324091e5051c6486efe25e2ec7e5b9
aliyeganehfard <76558514+aliyeganehfard@users.noreply.github.com>
2023-08-14 18:34:11 +0330

    #6

---
+++ LARxury/core-service/src/test/java/ir/larxury/core/service/CoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.larxury.core.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
