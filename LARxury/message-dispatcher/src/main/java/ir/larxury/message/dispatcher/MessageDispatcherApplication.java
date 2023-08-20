bdfc44b707267ed45c25377762dccca858cf21d4
aliyeganehfard <76558514+aliyeganehfard@users.noreply.github.com>
2023-08-20 19:00:38 +0330

    #20

---
+++ LARxury/message-dispatcher/src/main/java/ir/larxury/message/dispatcher/MessageDispatcherApplication.java
@@ -0,0 +1,13 @@
+package ir.larxury.message.dispatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageDispatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageDispatcherApplication.class, args);
+	}
+
+}
---
+++ LARxury/message-dispatcher/src/test/java/ir/larxury/message/dispatcher/MessageDispatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.larxury.message.dispatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageDispatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
