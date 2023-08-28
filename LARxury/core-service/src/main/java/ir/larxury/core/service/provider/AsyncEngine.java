c1cbd9262aacf7ad7374bfb1d2668ac5ee22b7d9
aliyeganehfard <76558514+aliyeganehfard@users.noreply.github.com>
2023-08-28 18:13:48 +0330

    #39

--- LARxury/core-service/src/main/java/ir/larxury/core/service/provider/AsyncEngine.java
+++ LARxury/core-service/src/main/java/ir/larxury/core/service/provider/AsyncEngine.java
@@ -1,9 +1,10 @@
 package ir.larxury.core.service.provider;
 
 import ir.larxury.common.utils.common.aop.ErrorCode;
-import ir.larxury.core.service.common.dto.authService.res.UserInfoRes;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +12,7 @@ import java.util.concurrent.CompletableFuture;
 
 @Slf4j
 @Service
+@PropertySource(value = "classpath:core-service.properties")
 public class AsyncEngine {
 
     @Autowired
@@ -19,21 +21,26 @@ public class AsyncEngine {
     @Autowired
     private AuthServiceProvider authServiceProvider;
 
+    @Value("${core.service.notifier.enabled}")
+    private Boolean isNotifierEnable;
+
     @Async
     public void sendEmailInstantDeliveryMessage(String userId, String subject, String message) {
-        CompletableFuture.runAsync(() -> {
-            try {
-                var userInfo = authServiceProvider.getUserInfo(userId);
-                messageDispatcherProvider.instantDelivery(subject, message, userInfo.getEmail());
-            } catch (Exception ex) {
-                log.error(ErrorCode.CORE_SERVICE_TROUBLE_TO_SEND_INSTANT_DELIVERY.getTechnicalMessage());
-                ex.printStackTrace();
-            }
-        }).whenComplete((t, u) -> {
-            if (u != null) {
-                log.error(ErrorCode.CORE_SERVICE_TROUBLE_TO_SEND_INSTANT_DELIVERY.getTechnicalMessage());
-                u.printStackTrace();
-            }
-        });
+        if (isNotifierEnable) {
+            CompletableFuture.runAsync(() -> {
+                try {
+                    var userInfo = authServiceProvider.getUserInfo(userId);
+                    messageDispatcherProvider.instantDelivery(subject, message, userInfo.getEmail());
+                } catch (Exception ex) {
+                    log.error(ErrorCode.CORE_SERVICE_TROUBLE_TO_SEND_INSTANT_DELIVERY.getTechnicalMessage());
+                    ex.printStackTrace();
+                }
+            }).whenComplete((t, u) -> {
+                if (u != null) {
+                    log.error(ErrorCode.CORE_SERVICE_TROUBLE_TO_SEND_INSTANT_DELIVERY.getTechnicalMessage());
+                    u.printStackTrace();
+                }
+            });
+        }
     }
 }
